3337e34c26dfc15f0484ad431acbdfec74551fa1
sayuru99 <saurudealwis@ymail.com>
2022-12-11 04:07:40 +0530

    initial commit

---
+++ BookListRazor/Pages/BookList/Create.cshtml.cs
@@ -0,0 +1,42 @@
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookListRazor.Pages.BookList
+{
+    public class CreateModel : PageModel
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+
+        public Book Book { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            if(ModelState.IsValid)
+            {
+                await _db.Book.AddAsync(Book);
+                await _db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return Page();
+
+            }
+
+        }
+    }
+}
